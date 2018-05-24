8e938ca102bf43e5bb31256a42d8c5f67c9a610c
Simon Thomas <simonpthomas89@gmail.com>
2018-05-24 21:18:03 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,4 @@
 # Write your #display_rainbow method here
-red = [R: red]
-orange =[O:orange]
-yellow = [Y: yellow]
-green = [G: green]
-blue = [B: blue]
-indigo = [I: indigo]
-violet = [V: violet]
 
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
